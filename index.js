403ad8d895e1245047aad88fa5bb5f8ac926b37d
JoseJoel <josejoelms@gmail.com>
2022-03-25 20:02:15 -0400

    Added a console log to track the application flow

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.post("/sms", (req, res) =>{
     res.end(sResponse + "</Response>");
 });
 
+console.log("Code execution reached here");
+
 var port = process.env.PORT || parseInt(process.argv.pop()) || 3002;
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
